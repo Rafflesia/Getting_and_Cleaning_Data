4d67f2d7f09bbe96fd35a5052907bf6a897cce07
Rafflesia <Rafflesia@users.noreply.github.com>
2015-08-23 22:36:07 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -9,11 +9,9 @@ if (!require("data.table")) {
    install.packages("data.table") 
  } 
  
- 
  if (!require("reshape2")) { 
    install.packages("reshape2") 
  } 
- 
  
  require("data.table") 
  require("reshape2") 
