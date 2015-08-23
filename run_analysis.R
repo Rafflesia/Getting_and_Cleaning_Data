1e69cced1cce626b519fc81aa157635e68c51be9
Rafflesia <Rafflesia@users.noreply.github.com>
2015-08-23 22:30:00 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,9 +2,66 @@
 ## 2.Extracts only the measurements on the mean and standard deviation for each measurement. 
 ## 3.Uses descriptive activity names to name the activities in the data set
 ## 4.Appropriately labels the data set with descriptive variable names. 
-##5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+## 5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-library(data.table)
-library(dplyr)
+setwd("D:/Coursera/Module3")
+if (!require("data.table")) { 
+   install.packages("data.table") 
+ } 
+ 
+ 
+ if (!require("reshape2")) { 
+   install.packages("reshape2") 
+ } 
+ 
+ 
+ require("data.table") 
+ require("reshape2") 
 
+
+featureNames <- read.table("UCI HAR Dataset/features.txt")
+activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt", header = FALSE)
+
+subjectTrain <- read.table("UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+activityTrain <- read.table("UCI HAR Dataset/train/y_train.txt", header = FALSE)
+featuresTrain <- read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE)
+
+subjectTest <- read.table("UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+activityTest <- read.table("UCI HAR Dataset/test/y_test.txt", header = FALSE)
+featuresTest <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE)
+
+
+## Part 1 - Merges the training and the test sets to create one data set.
+subject <- rbind(subjectTrain, subjectTest)
+activity <- rbind(activityTrain, activityTest)
+features <- rbind(featuresTrain, featuresTest)
+
+colnames(features) <- t(featureNames[2])
+
+colnames(activity) <- "Activity"
+colnames(subject) <- "Subject"
+completeData <- cbind(features,activity,subject)
+
+## Part 2 - Extracts only the measurements on the mean and standard deviation for each measurement. 
+columnsWithMeanSTD <- grep(".*Mean.*|.*Std.*", names(completeData), ignore.case=TRUE)
+
+requiredColumns <- c(columnsWithMeanSTD, 562, 563)
+dim(completeData)
+## [1] 10299   563
+
+extractedData <- completeData[,requiredColumns]
+dim(extractedData)
+## [1] 10299    88
+
+
+## Part 3 - Uses descriptive activity names to name the activities in the data set
+extractedData$Activity <- as.character(extractedData$Activity)
+for (i in 1:6){
+extractedData$Activity[extractedData$Activity == i] <- as.character(activityLabels[i,2])
+}
+
+extractedData$Activity <- as.factor(extractedData$Activity)
+
+## Part 4 - Appropriately labels the data set with descriptive variable names. 
+names(extractedData)
 
